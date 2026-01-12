176fb5e18fe8364197571b69ebea6b2874e56ea4
chengyifei <2389467575@qq.com>
2026-01-12 10:00:05 +0000

    删除微调智谱6B的代码

--- ChatGLM-6B/download_model.py
+++
@@ -1,6 +0,0 @@
-from modelscope import snapshot_download, AutoTokenizer
-from transformers import AutoModelForCausalLM, TrainingArguments, Trainer, DataCollatorForSeq2Seq
-import torch
-
-# 在modelscope上下载Qwen模型到本地目录下
-model_dir = snapshot_download("ZhipuAI/chatglm3-6b", cache_dir="./", revision="master")
